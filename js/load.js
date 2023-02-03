204e5c998b134e3c4e094b43eed683b4da655a19
HMNConsulting <entropicbit@protonmail.com>
2023-02-03 00:37:08 -0500

    Added bech32 encoding for npubs

--- js/load.js
+++ js/load.js
@@ -1,3 +1,14 @@
+function fromHexString(str) {
+      if (str.length % 2 !== 0 || !/^[0-9a-f]+$/i.test(str)) {
+        return null;
+      }
+      let buffer = new Uint8Array(str.length / 2);
+      for (let i = 0; i < buffer.length; i++) {
+        buffer[i] = parseInt(str.substr(2 * i, 2), 16);
+      }
+      return buffer;
+}
+
 async function load() {
 	let url = 'https://nostr.haus/.well-known/nostr.json';
 	let obj;
@@ -14,17 +25,16 @@ async function load() {
 	let output = "";
 	for (const name in plebs) {
 		let key = plebs[name];
+		let buffer = fromHexString(key);
+		let npub = bech32.encode('npub', buffer, "bech32");
 
-		output +=
-			`
-			<div class="row">
-			<span class="name"> ${name}@nostr.haus</span><br />
-			<span class=\"key\"> ${key} </span>
-			</div>
-			`
+		output +=	`
+					<div onclick="window.open('https://snort.social/p/${npub}')" class="row">
+					<span class="name"><i class="fa-solid fa-user-plus"></i> ${name}@nostr.haus</span><br />
+					<span class=\"key\"> ${npub} </span>
+					</div>
+					`
 	}
-
-	//console.log(output);
 	
-	document.getElementById('list').innerHTML = output;
+	document.querySelector("#list").innerHTML = output;
 }
